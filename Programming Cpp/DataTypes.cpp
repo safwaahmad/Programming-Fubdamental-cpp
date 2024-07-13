6014c1f0be0068ce0855687086190b541e535f51
safwaahmad <sahmedsaffi@gmail.com>
2024-07-13 14:45:56 +0500

    add

---
+++ Programming Cpp/DataTypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>   
+using namespace std ; 
+int main()            
+{
+    int number = 32;
+    char character = 'a';
+    float floatpoint = 3.456;
+    double Double = 12345;
+    bool bool1 = true ;
+    
+    cout<<number<<endl;
+    cout<<character<<endl;
+    cout<<floatpoint<<endl;
+    cout<<Double<<endl;
+    cout<<bool1<<endl;
+}
