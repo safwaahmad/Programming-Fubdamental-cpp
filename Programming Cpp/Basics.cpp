1162841b45ba47ee410a1dd7bcfc7673b86bd5b2
safwaahmad <sahmedsaffi@gmail.com>
2024-07-13 14:31:42 +0500

    add

---
+++ Programming Cpp/Basics.cpp
@@ -0,0 +1,13 @@
+#include<iostream>   
+using namespace std ; 
+int main()            
+{
+    //using multiple cout
+    cout<<("*")<<endl;
+    cout<<("**")<<endl;
+    cout<<("***")<<endl;
+    cout<<("-------------------\n");
+    //using one cout
+    cout<<("*\n**\n***\n");
+
+}
--- Programming Cpp/First.cpp
+++ Programming Cpp/First.cpp
@@ -1,9 +1,10 @@
-//Libraray--header
+//Libraray--header file
 #include<iostream>    //preprocessor directive-> #
 using namespace std ; // stored in ram in std
 //----function----
 int main()            //main point
 {
 cout<<("Hello World!!!\n"); //output
+cout<<("Hello World!!!")<<endl; //endl is for new line
 cout<<("\t This is my First programme"); //(\n is for new line-\t is for tab)
 }
