d4a7b79a22e76d9b2debd23e5d3bdf5d432d55ea
safwaahmad <sahmedsaffi@gmail.com>
2024-07-13 14:59:12 +0500

    add

---
+++ Programming Cpp/sizeoff.cpp
@@ -0,0 +1,20 @@
+#include<iostream>   
+using namespace std ; 
+int main()            
+{
+    int number = 32;
+    char character = 'a';
+    float floatpoint = 3.456;
+    double Double = 12345;
+    bool bool1 = true ;
+    short shortt = 10;
+    long long1 = 11000;
+    cout<<"The Size of integer is  :    "<<sizeof(number)<<endl;
+    cout<<"The Size of charactr is :    "<<sizeof(character)<<endl;
+    cout<<"The Size of flaot is    :    "<<sizeof(floatpoint)<<endl;
+    cout<<"The Size of double is   :    "<<sizeof(Double)<<endl;
+    cout<<"The Size of bool is     :    "<<sizeof(bool1)<<endl;
+    cout<<"The Size of short is    :    "<<sizeof(shortt)<<endl;
+    cout<<"The Size of long is    :     "<<sizeof(long1)<<endl;
+}
+   
