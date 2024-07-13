1b0540867bedd4fe7bd910b196cb44f65103dbdb
safwaahmad <sahmedsaffi@gmail.com>
2024-07-13 15:35:56 +0500

    add

---
+++ input/output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>   
+using namespace std ; 
+int main()            
+{
+int number1;
+cout<<("Enter the number1 : ")<<endl;
+cin>>number1;
+int number2;
+cout<<("Enter the number2 : ")<<endl;
+cin>>number2;
+int sum;
+cout<<("Result of both numbers is : ")<<endl;
+sum = number1+number2;
+cout<<sum;
+}
