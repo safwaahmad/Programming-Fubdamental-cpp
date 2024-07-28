2821e4979cdfbf9aa9b2e861adf87ab0f7e11dec
safwaahmad <sahmedsaffi@gmail.com>
2024-07-28 13:24:11 +0500

    add

--- Programming Cpp/First.cpp
+++ Programming Cpp/First.cpp
@@ -4,6 +4,7 @@ using namespace std ; // stored in ram in std
 //----function----
 int main()            //main point
 {
+    
 cout<<("Hello World!!!\n"); //output
 cout<<("Hello World!!!")<<endl; //endl is for new line
 cout<<("\t This is my First programme"); //(\n is for new line-\t is for tab)
